86355d07fbc3eddcaf33c194e0af8c0e504f58c4
Nazmul Huda <mhnazmul48@gmail.com>
2020-10-15 21:13:50 +0600

    React Notes Backupt in Github

---
+++ dumy Data/index.js
@@ -0,0 +1,110 @@
+const tutorial=[
+    {
+        title: "The Complete Android Oreo Developer Course - Build 23 Apps!",
+        instructor: "Billal Hossain",
+        price: 10,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/gzBp5mv/android.jpg"
+    },
+
+    {
+        title: "Bootstrap 4 From Scratch With 5 Projects",
+        instructor: "Rashedul Alam",
+        price: 15,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/k6KXj2h/bootstrap.png"
+    },
+    
+    {
+        title: "C Programming For Beginners",
+        instructor: "Hafizul Hayder",
+        price: 23,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/cwG14cC/c.png"
+    },
+    {
+        title: "Dart - The Complete Guide",
+        instructor: "Rawnok Tanjil",
+        price: 17,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/zQJJSsV/dart.jpg"
+    },
+    {
+        title: "ES6 From Scrach",
+        instructor: "Hasin Hayder",
+        price: 17,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/RpBw0Gs/es6.jpg"
+    },
+    {
+        title: "Java From Zero To Hero",
+        instructor: "Khalid Farhan",
+        price: 27,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/y4VB2bD/java.jpg"
+    },
+    {
+        title: "Laravel For Beginers",
+        instructor: "Rabbil Hasan",
+        price: 20,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/HH5Twsv/laravel.jpg"
+    },
+    {
+        title: "Lumen Rest API (Application Porgramming Interface)",
+        instructor: "Pranto Roy",
+        price: 10,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/VpfZtzt/lumen.jpg"
+    },
+    {
+        title: "Project From Scrach Using Flutter",
+        instructor: "Ewuwin Diaz",
+        price: 17,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/KykD4zW/flutter.jpg"
+    },
+
+    {
+        title: "HTML Within 1 Day",
+        instructor: "Delwar Jahan",
+        price: 10,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/Wkr1Jc5/html.png"
+    },
+    {
+        title: "React Naitve For Beginers",
+        instructor: "FA Nasim",
+        price: 20,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/Jkfwc3w/native.jpg"
+    },
+    {
+        title: "Audio Book Using Python 12 minit",
+        instructor: "Jankar Mahabub",
+        price: 10,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/CWYZLsC/python.jpg"
+    },
+    {
+        title: "SPA (Single Page Application) Usin React JS",
+        instructor: "Sahin Alam",
+        price: 27,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/cwVfSG3/react.jpg"
+    },
+    {
+        title: "Learn Ruby Programming 2020",
+        instructor: "Mahabubul Alam",
+        price: 10,
+        ratting: "Bestseller",
+        image: "https://i.ibb.co/wYN59KC/ruby.jpg"
+    },
+    {
+        title: "Dynamic CSS Using SASS",
+        instructor: "Fakar Uddin",
+        price: 12,
+        ratting: "Newseller",
+        image: "https://i.ibb.co/d5rZ9T4/sass.png"
+    }
+]
